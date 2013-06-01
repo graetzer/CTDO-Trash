4eb19c3845590623b9d1911461225a68494148ff
Simon Grätzer <simon@graetzer.org>
2013-06-01 13:42:44 +0200

    Initial Commit

---
+++ CTDO Trash/SGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SGAppDelegate.h
+//  CTDO Trash
+//
+//  Created by Simon Grätzer on 01.06.13.
+//  Copyright (c) 2013 Simon Peter Grätzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CTDO Trash/SGViewController.m
@@ -0,0 +1,29 @@
+//
+//  SGViewController.m
+//  CTDO Trash
+//
+//  Created by Simon Grätzer on 01.06.13.
+//  Copyright (c) 2013 Simon Peter Grätzer. All rights reserved.
+//
+
+#import "SGViewController.h"
+
+@interface SGViewController ()
+
+@end
+
+@implementation SGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
